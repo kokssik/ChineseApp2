9717698738fd317a7bd95f3052bcb52f05ae1eba
kokssik <rudolf.sekac@gmail.com>
2023-06-11 19:52:53 +0200

    lessons++

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ const config = {
 
   plugins: [],
 
+  safelist: [
+    'bg-red-500',
+    'text-3xl',
+    'lg:text-4xl',
+    'bg-blue-200',
+    'border-3',
+    'border-solid',
+    'border-black',
+    'px-5',
+    'py-3',
+    'text-lg',
+    'font-medium',
+  ],
+
   corePlugins: {
     preflight: false,
   }
