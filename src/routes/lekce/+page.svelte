c66e2d2e88b515035596ea3df68d35d0daf9f4d6
kokssik <rudolf.sekac@gmail.com>
2023-06-11 15:18:06 +0200

    added lessons

--- src/routes/lekce/+page.svelte
+++ src/routes/lekce/+page.svelte
@@ -8,23 +8,31 @@
     <div class="font-sans text-4xl font-bold mb-5">Seznam lekcí:</div>
     <Seznam
       nadpis="Lekce 1"
-      odkaz="/lekce/5656"
-      subs={["Výslovnost a přepis čínštiny", "Pinyin", "Český Pchin-jin"]}
+      odkaz="/lekce/1"
+      subs={[
+        "Základní větná stavba a sloveso být",
+        "Pořadí slov ve větě",
+        "Jednoduché věty",
+      ]}
     />
     <Seznam
       nadpis="Lekce 2"
-      odkaz="/lekce/69"
-      subs={["Základní větná stavba", "Pořadí slov ve větě", "Jednoduchá věta"]}
+      odkaz="/lekce/2"
+      subs={["Věty s přídavnými jmény", "很"]}
     />
-    <Seznam nadpis="Lekce 3" odkaz="/lekce/69" subs={["Sloveso být", "Shi"]} />
+    <Seznam
+      nadpis="Lekce 3"
+      odkaz="/lekce/3"
+      subs={["Jak se zaptat na otázku", "吗", "呢"]}
+    />
     <Seznam
       nadpis="Lekce 4"
       odkaz="/lekce/5656"
-      subs={["'Velmi, příliš'", "tai"]}
+      subs={["Příliš", "Příliš se používá jinak", "太"]}
     />
     <Seznam
       nadpis="Lekce 5"
-      odkaz="/lekce/69"
+      odkaz="/lekce/4"
       subs={["Používání přídavných jmen", "Jednoduché věty s přídavnými jmény"]}
     />
   </div>
